6c8fb00e62456ae3771727c7cf4b9aa3e48c1853
Jack Haycock <jhaycock@ptla.org>
2023-08-28 09:50:03 -0400

    Re-labeled fee waiver form to align P/D name questions

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.Feewaiverapp',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.13.0'],
+      install_requires=['docassemble.AssemblyLine>=2.24.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/Feewaiverapp/', package='docassemble.Feewaiverapp'),
      )
